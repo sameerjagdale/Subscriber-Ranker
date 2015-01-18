f474600a4447c090c7ac90139c6ae6a81752c66e
sameerjagdale <sameer.jagdale@mail.mcgill.ca>
2015-01-17 23:35:19 -0500

    Added methods to SubscriberAcessInfo
    Added getter setter methods as well as hashcode, toString and equals methods.

--- subscriber/SubscriberAccessInfo.java
+++ subscriber/SubscriberAccessInfo.java
@@ -1,4 +1,8 @@
 package subscriber;
+
+import java.util.HashMap;
+import java.io.*;
+
 public class SubscriberAccessInfo {
 	private SubscriberInfo subInfo;
 	private String timestamp;
@@ -8,10 +12,75 @@ public class SubscriberAccessInfo {
 		timestamp = time;
 	}
 	
-	public static SubscriberAccessInfo genSubscriberAccessInfo(String tsvLine) {
-		String fields[] = tsvLine.split(" ");
-		return new SubscriberAccessInfo(new SubscriberInfo(fields[0], fields[1], 
-			Integer.parseInt(fields[2])), fields[3]);
+	public void setSubInfo(SubscriberInfo info) {
+		subInfo = info;
 	}
+	
+	public SubscriberInfo getSubInfo() {
+		return subInfo;
+	}
+	
+	public void setTimestamp(String time) {
+		timestamp = time;
+	}
+	
+	public String getTimestamp() {
+		return timestamp;
+	}
+	
+	@Override 
+	public String toString() {
+		return subInfo.toString() + "\t" + timestamp;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null)  return false;
+		if(!(obj instanceof SubscriberAccessInfo)) return false;
+		SubscriberAccessInfo info = (SubscriberAccessInfo)obj;
+		return subInfo.equals(info.getSubInfo()) &&
+			timestamp.equals(info.getTimestamp());
+	}
+
+	@Override
+	public int hashCode() {
+		return subInfo.hashCode() + timestamp.hashCode();
+	}
+
+	public static SubscriberAccessInfo genSubscriberAccessInfo(String tsvLine) {
+		String fields[] = tsvLine.split("[ \t]+");
+		return new SubscriberAccessInfo(new SubscriberInfo(fields[0], fields[2], 
+			Integer.parseInt(fields[1]), fields[3].substring(0,8)), fields[3].substring(8));
+	}
+	
+	public static void main(String args[]) {
+			if(args.length < 1) {
+					System.out.println("No File name provided");
+			}
+			try {	
+					BufferedReader br =  new BufferedReader(new FileReader((new File(args[0])).
+											getAbsolutePath()));
+					HashMap<SubscriberAccessInfo, Integer> map =  new HashMap<SubscriberAccessInfo, Integer>();
+					for(int i = 0; i < 20; i++) {
+							SubscriberAccessInfo info  = genSubscriberAccessInfo(br.readLine());
+							// System.out.println(info.toString());
+							if(map.containsKey(info)) {
+									System.out.println("Match Found");
+									map.put(info,Integer.valueOf(map.get(info).intValue() + 1));
+							} else {
+									map.put(info, new Integer(1));
+							}
+					}
+
+					for( SubscriberAccessInfo info : map.keySet()) {
+						System.out.println(info + " " +  map.get(info).toString());
+					}
+			} catch(IOException e) {
+				e.printStackTrace();
+			} 
+
+	}
+
+
 }
 
