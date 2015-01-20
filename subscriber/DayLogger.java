86e87d26ca27c9c3738263f69a45a128614c22be
sameerjagdale <sameer.jagdale@mail.mcgill.ca>
2015-01-20 18:09:55 -0500

    Added a class to be used as a key for the final map.

---
+++ subscriber/DayLogger.java
@@ -0,0 +1,7 @@
+package subscriber;
+import java.util.HashMap;
+import java.io.*;
+import org.apache.hadoop.io.WritableComparable;
+
+public class DayLogger implements WritableComparable<DayLogger> {
+}
