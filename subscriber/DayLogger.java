47c05b48a75f08c7860cda4e0d21dd94dd4171dd
sameerjagdale <sameer.jagdale@mail.mcgill.ca>
2015-01-20 18:29:04 -0500

    Added override annotations and added equals and hashCode functions.

--- subscriber/DayLogger.java
+++ subscriber/DayLogger.java
@@ -6,16 +6,20 @@ import org.apache.hadoop.io.WritableComparable;
 public class DayLogger implements WritableComparable<DayLogger> {
 	private String date;
 	private int count;
+	
+	@Override
 	public void write(DataOutput out) throws IOException {
 		out.writeUTF(date);
 		out.writeInt(count);
 	}
-	
+
+	@Override	
 	public void readFields(DataInput in) throws IOException {
 		date = in.readUTF();
 		count = in.readInt();
 	}
 	
+	@Override
 	public int compareTo(DayLogger other) {
 		if(date.compareTo(other.getDate()) == 0) {
 			return (new Integer(count)).compareTo(new Integer(other.getCount()));
@@ -23,6 +27,7 @@ public class DayLogger implements WritableComparable<DayLogger> {
 		return date.compareTo(other.getDate());
 	}
 	
+	@Override 	
 	public String toString() {
 		return date + "\t" + count;
 	}
@@ -51,5 +56,23 @@ public class DayLogger implements WritableComparable<DayLogger> {
 
 	public void setCount() {
 		this.count = count;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null) {
+			return false;
+		}
+		if(!(obj instanceof DayLogger)) {
+			return false;
+		}
+		DayLogger otherDay = (DayLogger) obj;
+		return date.equals(otherDay.getDate()) &&
+			(new Integer(count)).equals(new Integer(count));
+	}
+	
+	@Override 
+	public int hashCode() {
+		return date.hashCode() + (new Integer(count)).hashCode();
 	}
 }
