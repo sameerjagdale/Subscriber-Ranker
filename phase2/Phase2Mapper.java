bd705344c9b59b561d5b74f6843adc4293120042
sameerjagdale <sameer.jagdale@mail.mcgill.ca>
2015-01-23 23:46:14 -0500

    Added documentation to the phase 2 mapper.

--- phase2/Phase2Mapper.java
+++ phase2/Phase2Mapper.java
@@ -12,10 +12,24 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import subscriber.*;
 
+/** 
+ * Class implements the map operation for the second phase of the map
+ * operation. It reads entire lines from the file and outputs the SubscriberInfo object 
+ * as key and and an IntWritable as value.
+ * @author Sameer Jagdale.
+ */
 public class Phase2Mapper extends
 		Mapper<Object, Text, SubscriberInfo, IntWritable> {
+/** 
+ * one. static variable initialised to one which is used as the output value of the map
+ * operation. 
+ */
 	private static IntWritable one = new IntWritable(1);
 
+/** 
+ * map. The method is overriden from the Mapper class. Implements the map functionality 
+ * of phase 2. 
+ */
 	public void map(Object key, Text value, Context context)
 			throws IOException, InterruptedException {
 		context.write(SubscriberInfo.genSubscriberInfo(value.toString()), one);
