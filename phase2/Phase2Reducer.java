43abae60201d2686f90b6164096e1b2540919974
sameerjagdale <sameer.jagdale@mail.mcgill.ca>
2015-01-23 23:54:13 -0500

    Added documentation for the phase2 reducer.

--- phase2/Phase2Reducer.java
+++ phase2/Phase2Reducer.java
@@ -12,10 +12,24 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import subscriber.*;
 
+/** 
+ * Class implements the phase 2 reducer for the program. 
+ * Generates a count of the number of unique timestamps for a single day for a single 
+ * key. 
+ * @author Sameer Jagale
+ */
 public class Phase2Reducer extends
 		Reducer<SubscriberInfo, IntWritable, SubscriberInfo, IntWritable> {
+/** 
+ * result. Holds the count of the number of unique time stamps for a day for every 
+ * subscriber. 
+ */
 	private IntWritable result = new IntWritable();
-
+/** 
+ * The method is overriden from the Reducer class. Implements the reduce functionality of
+ * phase 2.
+ */
+	@Overriden
 	public void reduce(SubscriberInfo key, Iterable<IntWritable> values,
 			Context context) throws IOException, InterruptedException {
 		int sum = 0;
