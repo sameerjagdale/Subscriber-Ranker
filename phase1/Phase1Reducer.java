526910d260ad67bbcec6eb88dcbadb186bf7f074
sameerjagdale <sameer.jagdale@mail.mcgill.ca>
2015-01-23 23:46:58 -0500

    Added documentation to the phase1 reducer.

--- phase1/Phase1Reducer.java
+++ phase1/Phase1Reducer.java
@@ -11,12 +11,26 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import subscriber.*;
-
+/** 
+ * Reducer class for the first phase of the program. It reads objects of type
+ * SubscriberAccessInfo from the map function and returns a count of the number of
+ * non-unique timestamps for the current key. 
+ * @author Sameer Jagdale
+ */
 public class Phase1Reducer
 		extends
 		Reducer<SubscriberAccessInfo, IntWritable, SubscriberAccessInfo, IntWritable> {
+/**
+ * result. Static variable which holds the count of the number of the non-unique 
+ * timestamps for each key. 
+ */
 	private IntWritable result = new IntWritable();
 
+/** 
+ * The method is overriden from the Reducer class. Implements the functionality for the 
+ * reduce operation. 
+ */
+ 	@Overriden
 	public void reduce(SubscriberAccessInfo key, Iterable<IntWritable> values,
 			Context context) throws IOException, InterruptedException {
 		int sum = 0;
